51211ea677493718fe7f20acd61b85f58466e84f
artem pecheny <pecheny@gmail.com>
2013-12-26 07:20:12 +0600

    Completion done

--- src/pgr/gconsole/GConsole.hx
+++ src/pgr/gconsole/GConsole.hx
@@ -38,7 +38,7 @@ class GConsole extends Sprite {
 	private var _isMonitorOn:Bool;
 	private var _isConsoleOn:Bool;
 	private var _isCompleting:Bool;
-	private var _completionMainPart:String;
+	private var _completionMainPart:String = "";
 
 	private static var commandNames:Array<String> = { ["clear", "monitor", "help", "commands", "vars", "fcns", "set"];};
 
@@ -68,62 +68,53 @@ class GConsole extends Sprite {
 		GameConsole.log("~~~~~~~~~~ GAME CONSOLE ~~~~~~~~~~");
 	}
 
-	public function setConsoleFont(font:String = null, embed:Bool = true, size:Int = 14, bold:Bool = false, italic:Bool = false, underline:Bool = false )
-	{
+	public function setConsoleFont(font:String = null, embed:Bool = true, size:Int = 14, bold:Bool = false, italic:Bool = false, underline:Bool = false) {
 		_interface.setConsoleFont(font, embed, size, bold, italic, underline);
 	}
 
-	public function setPromptFont(font:String = null, embed:Bool = true, size:Int = 16, yOffset:Int = 22, bold:Bool = false, ?italic:Bool = false, underline:Bool = false )
-	{
+	public function setPromptFont(font:String = null, embed:Bool = true, size:Int = 16, yOffset:Int = 22, bold:Bool = false, ?italic:Bool = false, underline:Bool = false) {
 		_interface.setPromptFont(font, embed, size, yOffset, bold, italic, underline);
 	}
 
-	public function setMonitorFont(font:String = null, embed:Bool = true, size:Int = 14, bold:Bool = false, ?italic:Bool = false, underline:Bool = false )
-	{		
+	public function setMonitorFont(font:String = null, embed:Bool = true, size:Int = 14, bold:Bool = false, ?italic:Bool = false, underline:Bool = false) {
 		_interface.setMonitorFont(font, embed, size, bold, italic, underline);
 	}
 
-	public function showConsole() 
-	{
+	public function showConsole() {
 		if (!this.visible) return;
 		Lib.current.stage.focus = _interface.txtPrompt;
 		_isConsoleOn = true;
 		_interface.toggleConsoleOn(true);
 	}
 
-	public function hideConsole()
-	{
+	public function hideConsole() {
 		if (!this.visible) return;
 		_isConsoleOn = false;
 		_interface.toggleConsoleOn(false);
 	}
 
-	public function showMonitor() 
-	{
+	public function showMonitor() {
 		_isMonitorOn = true;
 		_interface.toggleMonitor(true);
 		addEventListener(Event.ENTER_FRAME, updateMonitor);
-		
+
 		_elapsedFrames = _monitorRate + 1;
-		updateMonitor(null);	// renders first frame.
+		updateMonitor(null); // renders first frame.
 	}
 
-	public function hideMonitor()
-	{
+	public function hideMonitor() {
 		_isMonitorOn = false;
 		_interface.toggleMonitor(false);
 		removeEventListener(Event.ENTER_FRAME, updateMonitor);
 	}
 
-	public function enable() 
-	{
+	public function enable() {
 		visible = true;
 		Lib.current.stage.addEventListener(KeyboardEvent.KEY_UP, onKeyUp, false);
 		Lib.current.stage.addEventListener(KeyboardEvent.KEY_DOWN, onKeyDown, false);
 	}
 
-	public function disable() 
-	{
+	public function disable() {
 		if (_isMonitorOn) hideMonitor();
 		if (_isConsoleOn) hideConsole();
 		Lib.current.stage.removeEventListener(KeyboardEvent.KEY_UP, onKeyUp, false);
@@ -131,43 +122,38 @@ class GConsole extends Sprite {
 		visible = false;
 	}
 
-	public function setShortcutKeyCode(key:Int)
-	{
+	public function setShortcutKeyCode(key:Int) {
 		_consoleScKey = key;
 	}
-	
-	public function log(data:Dynamic) 
-	{
+
+	public function log(data:Dynamic) {
 		if (data == "") return;
 		_firstLine ?
-			_firstLine = false : 
-			_interface.txtConsole.text += '\n'; //add new line if console text was not clear.
-		
+		_firstLine = false :
+		_interface.txtConsole.text += '\n'; //add new line if console text was not clear.
+
 		_interface.txtConsole.text += data.toString();
 		_interface.txtConsole.scrollV = _interface.txtConsole.maxScrollV;
 	}
 
-	public function registerVariable(object:Dynamic, name:String, alias:String, monitor:Bool=false) 
-	{
+	public function registerVariable(object:Dynamic, name:String, alias:String, monitor:Bool = false) {
 		if (!Reflect.isObject(object)) {
-		throw GC_TRC_ERR + "dynamic passed with the field: " + name + " is not an object.";
+			throw GC_TRC_ERR + "dynamic passed with the field: " + name + " is not an object.";
 			return;
 		}
-		#if !(cpp || neko)
-		if (!Reflect.hasField(object, name)) 
-		#else
+#if !(cpp || neko)
+		if (!Reflect.hasField(object, name))
+#else
 		if (Reflect.getProperty(object, name) == null) 
-		#end
-		{
-			throw  GC_TRC_ERR + name + " field was not found in object " + object + " passed.";
+		#end {
+			throw GC_TRC_ERR + name + " field was not found in object " + object + " passed.";
 			return;
 		}
-		
+
 		log(GCCommands.registerVariable(object, name, alias, monitor));
 	}
 
-	public function unregisterVariable(alias:String)
-	{
+	public function unregisterVariable(alias:String) {
 		if (GCCommands.unregisterVariable(alias)) {
 			log("variable " + alias + " unregistered.");
 		} else {
@@ -180,12 +166,11 @@ class GConsole extends Sprite {
 		if (!Reflect.hasField(object, name))
 #else
 		if (Reflect.getProperty(object, name) == null) 
-		#end
-		{
+		#end {
 			throw GC_TRC_ERR + name + " field was not found in object passed.";
 			return;
 		}
-		
+
 		if (!Reflect.isFunction(Reflect.field(object, name))) {
 			throw GC_TRC_ERR + "could not find function: " + name + " in object passed.";
 			return;
@@ -194,90 +179,82 @@ class GConsole extends Sprite {
 		log(GCCommands.registerFunction(object, name, alias, monitor, completionHandler));
 	}
 
-	public function unregisterFunction(alias:String)
-	{
+	public function unregisterFunction(alias:String) {
 		if (GCCommands.unregisterFunction(alias)) {
 			log("function " + alias + " unregistered.");
 		} else {
 			log("function " + alias + " not found.");
 		}
-		
+
 	}
-	
-	public function clearConsole() 
-	{
+
+	public function clearConsole() {
 		_interface.txtConsole.text = '';
 		_firstLine = true;
 	}
 
-	public function clearRegistry()
-	{
+	public function clearRegistry() {
 		GCCommands.clearRegistry();
 	}
-	
-	// INPUT HANDLING ------------------------------------------------------
-	
-	private function onKeyDown(e:KeyboardEvent):Void 
-	{
-		#if !(cpp || neko) // BUGFIX
-		if (_isConsoleOn) 
-			e.stopImmediatePropagation(); 
-		#end
+
+// INPUT HANDLING ------------------------------------------------------
+
+	private function onKeyDown(e:KeyboardEvent):Void {
+#if !(cpp || neko) // BUGFIX
+		if (_isConsoleOn)
+			e.stopImmediatePropagation();
+#end
 	}
-	
-	private function onKeyUp(e:KeyboardEvent):Void 
-	{
-		if (e.ctrlKey && cast(e.keyCode,Int) == _consoleScKey ) {
+
+	private function onKeyUp(e:KeyboardEvent):Void {
+		if (e.ctrlKey && cast(e.keyCode, Int) == _consoleScKey) {
 			_isMonitorOn ? hideMonitor() : showMonitor();
 			return;
 		}
-		
-		if (cast(e.keyCode,Int) == _consoleScKey) {
+
+		if (cast(e.keyCode, Int) == _consoleScKey) {
 			_isConsoleOn ? hideConsole() : showConsole();
 			return;
 		}
-		
-		
-		
-		// Read the following input if console is showing.
+
+
+// Read the following input if console is showing.
 		if (!_isConsoleOn)
-			return;	
-		
-		// Changed switch to if-else to avoid strande exception in haxe 3 regarding int and uint.
-		if (e.keyCode == 13)
-		{
+			return;
+
+		if (e.keyCode == Keyboard.TAB) {
+			completeInputLine();
+			return;
+		} else {
+			_isCompleting = false;
+			_completionMainPart = "";
+		}
+
+// Changed switch to if-else to avoid strande exception in haxe 3 regarding int and uint.
+		if (e.keyCode == 13) {
 			processInputLine();
 		}
-		else 
-		if (e.keyCode == 33)
-		{
-			_interface.txtConsole.scrollV -= _interface.txtConsole.bottomScrollV - _interface.txtConsole.scrollV +1;
+		else if (e.keyCode == 33) {
+			_interface.txtConsole.scrollV -= _interface.txtConsole.bottomScrollV - _interface.txtConsole.scrollV + 1;
 			if (_interface.txtConsole.scrollV < 0)
 				_interface.txtConsole.scrollV = 0;
 		}
-		else 
-		if (e.keyCode == 34)
-		{
-			_interface.txtConsole.scrollV += _interface.txtConsole.bottomScrollV - _interface.txtConsole.scrollV +1;
+		else if (e.keyCode == 34) {
+			_interface.txtConsole.scrollV += _interface.txtConsole.bottomScrollV - _interface.txtConsole.scrollV + 1;
 			if (_interface.txtConsole.scrollV > _interface.txtConsole.maxScrollV)
 				_interface.txtConsole.scrollV = _interface.txtConsole.maxScrollV;
 		}
-		else
-		if (e.keyCode == 38)
-		{
+		else if (e.keyCode == 38) {
 			nextHistory();
 		}
-		else 
-		if (e.keyCode == 40)
-		{
+		else if (e.keyCode == 40) {
 			prevHistory();
 		}
-		else
-		{
+		else {
 			_historyIndex = -1;
 		}
-		
-		/*
+
+/*
 		}
 		switch (e.keyCode) {
 			// ENTER
@@ -297,48 +274,45 @@ class GConsole extends Sprite {
 			default	:	_historyIndex = -1;
 		}
 		*/
-			
-		#if !(cpp || neko) // BUGFIX
+
+#if !(cpp || neko) // BUGFIX
 		e.stopImmediatePropagation(); // BUG - cpp issues.
-		#end
+#end
 	}
-	
-	private function prevHistory() 
-	{
+
+	private function prevHistory() {
 		_historyIndex--;
-		if (_historyIndex < 0 ) _historyIndex = 0;
-		if (_historyIndex > _historyArray.length - 1) return; 
-		
+		if (_historyIndex < 0) _historyIndex = 0;
+		if (_historyIndex > _historyArray.length - 1) return;
+
 		_interface.txtPrompt.text = _historyArray[_historyIndex];
-		#if !(cpp || neko)
-		_interface.txtPrompt.setSelection(_interface.txtPrompt.length, _interface.txtPrompt.length); 
-		#end
+#if !(cpp || neko)
+		_interface.txtPrompt.setSelection(_interface.txtPrompt.length, _interface.txtPrompt.length);
+#end
 	}
-	
-	private function nextHistory() 
-	{
+
+	private function nextHistory() {
 		if (_historyIndex + 1 > _historyArray.length - 1) return;
 		_historyIndex++;
-		
+
 		_interface.txtPrompt.text = _historyArray[_historyIndex];
-		#if !(cpp || neko)
-		_interface.txtPrompt.setSelection(_interface.txtPrompt.length, _interface.txtPrompt.length); 
-		#end
+#if !(cpp || neko)
+		_interface.txtPrompt.setSelection(_interface.txtPrompt.length, _interface.txtPrompt.length);
+#end
 	}
-	
-	// INPUT PARSE ----------------------------------------------------
-	
-	private function processInputLine() 
-	{
-		if (_interface.txtPrompt.text == '') 
+
+// INPUT PARSE ----------------------------------------------------
+
+	private function processInputLine() {
+		if (_interface.txtPrompt.text == '')
 			return;
 		var temp:String = _interface.txtPrompt.text;
-		
+
 		_historyArray.insert(0, _interface.txtPrompt.text);
 		_historyIndex = -1;
 		if (_historyArray.length > _historyMaxSz)
 			_historyArray.splice(_historyArray.length - 1, 1);
-		
+
 		log(": " + _interface.txtPrompt.text);
 		_interface.txtPrompt.text = '';
 		parseInput(temp);
@@ -350,9 +324,9 @@ class GConsole extends Sprite {
 		var argsLC:Array<String> = input.toLowerCase().split(' ');
 
 		switch (argsLC[0]) {
-			case "clear"	: clearConsole();
-			case "monitor"	: _isMonitorOn ? hideMonitor() : showMonitor();
-			case "help"		: log(GCCommands.showHelp());
+			case "clear" : clearConsole();
+			case "monitor" : _isMonitorOn ? hideMonitor() : showMonitor();
+			case "help" : log(GCCommands.showHelp());
 			case "commands" : log(GCCommands.showCommands());
 			case "vars" : log(GCCommands.listVars());
 			case "fcns" : log(GCCommands.listFunctions());
@@ -360,34 +334,32 @@ class GConsole extends Sprite {
 			default :log(GCCommands.callFunction(args));
 		}
 	}
-	
-	// MONITOR --------------------------------------------------------
 
-	private function updateMonitor(e:Event):Void 
-	{
+// MONITOR --------------------------------------------------------
+
+	private function updateMonitor(e:Event):Void {
 		_elapsedFrames++;
 		if (_elapsedFrames > _monitorRate) {
 			processMonitorOutput(GCCommands.getMonitorOutput());
 			_elapsedFrames = 0;
 		}
 	}
-	
-	private function processMonitorOutput(input:String) 
-	{
+
+	private function processMonitorOutput(input:String) {
 		if (input.length == 0) return;
 		var str1:String;
 		var str2:String;
-		
+
 		var splitPoint:Int = Std.int(input.length / 2) - 1;
-		
+
 		while (splitPoint < input.length) {
-			if (input.charAt(splitPoint) == "\n") 
+			if (input.charAt(splitPoint) == "\n")
 				break;
 			splitPoint++;
 		}
-		
+
 		_interface.txtMonitorLeft.text = input.substr(0, splitPoint);
-		_interface.txtMonitorRight.text = input.substr(splitPoint + 1); 
+		_interface.txtMonitorRight.text = input.substr(splitPoint + 1);
 	}
 // COMPLETION --------------------------------------------------------
 
@@ -398,9 +370,8 @@ class GConsole extends Sprite {
 		}
 		var temp:String = _interface.txtPrompt.text;
 		var args:Array<String> = temp.split(' ');
-		var argsLC:Array<String> = temp.toLowerCase().split(' ');
-		trace(commandNames);
-		var functions = GCCommands.getFunctionNames().concat(commandNames);
+		var functions = GCCommands.getFunctionNames();
+		functions = functions.concat(commandNames);
 		if (_completionMainPart == "") {
 			if ((functions.indexOf(args[0]) > -1)) {
 				_completionMainPart = temp;
@@ -432,7 +403,6 @@ class GConsole extends Sprite {
 		var alias = args.shift();
 		var mainPartArray = _completionMainPart.split(" ");
 		mainPartArray.shift();
-		trace(mainPartArray);
 		var mainPart = assembleString(mainPartArray);
 		var remoteObj:RemoteObj = GCCommands.getFunctionDescription(alias);
 		if (remoteObj != null && remoteObj.completion != null) {
@@ -449,7 +419,6 @@ class GConsole extends Sprite {
 	}
 
 	private function cycleRecords(list:Array<String>, current:String):String {
-		trace(list + " " + current);
 		var completed:String;
 		var currentOptIndex = list.indexOf(current);
 		if (currentOptIndex > -1) {
--- tests/ConsoleTest.hx
+++ tests/ConsoleTest.hx
@@ -8,86 +8,112 @@ import pgr.gconsole.GCInterface;
 import pgr.gconsole.GameConsole;
 import pgr.gconsole.GameConsole;
 class ConsoleTest {
-    var interfc:GCInterface;
-    var i:Int;
-    var f:Float;
-    var s:String;
+	var interfc:GCInterface;
+	var i:Int;
+	var f:Float;
+	var s:String;
 
-    @Before public function startup() {
-        GameConsole.init();
-        interfc = GConsole.instance._interface;
-        clearText();
-    }
+	@Before public function startup() {
+		GameConsole.init();
+		interfc = GConsole.instance._interface;
+		clearText();
+	}
+
 	@After public function tearDown():Void {
+		GameConsole.disable();
 		GConsole.instance = null;
 	}
 
-    @Test public function testInput():Void {
-        GameConsole.enable();
-        var str = "Test";
-        interfc.txtPrompt.text = str + "1";
-        Lib.current.stage.dispatchEvent(new KeyboardEvent(KeyboardEvent.KEY_UP, true, false, 0, 13)); // ENTER
-        GameConsole.showConsole();
-        GameConsole.disable();
-        GameConsole.showConsole();
-        interfc.txtPrompt.text = str + "2";
-        Lib.current.stage.dispatchEvent(new KeyboardEvent(KeyboardEvent.KEY_UP, true, false, 0, 13)); // ENTER
-        GameConsole.enable();
-        GameConsole.showConsole();
-        interfc.txtPrompt.text = str + "3";
-        Lib.current.stage.dispatchEvent(new KeyboardEvent(KeyboardEvent.KEY_UP, true, false, 0, 13)); // ENTER
-        GameConsole.hideConsole();
-        interfc.txtPrompt.text = str + "4";
-        Lib.current.stage.dispatchEvent(new KeyboardEvent(KeyboardEvent.KEY_UP, true, false, 0, 13)); // ENTER
-        var strings = interfc.txtConsole.text.split('\r');
-        Assert.areEqual(": Test3", strings[strings.length - 1]);
-    }
+	@Test public function testInput():Void {
+		GameConsole.enable();
+		var str = "Test";
+		interfc.txtPrompt.text = str + "1";
+		Lib.current.stage.dispatchEvent(new KeyboardEvent(KeyboardEvent.KEY_UP, true, false, 0, 13)); // ENTER
+		GameConsole.showConsole();
+		GameConsole.disable();
+		GameConsole.showConsole();
+		interfc.txtPrompt.text = str + "2";
+		Lib.current.stage.dispatchEvent(new KeyboardEvent(KeyboardEvent.KEY_UP, true, false, 0, 13)); // ENTER
+		GameConsole.enable();
+		GameConsole.showConsole();
+		interfc.txtPrompt.text = str + "3";
+		Lib.current.stage.dispatchEvent(new KeyboardEvent(KeyboardEvent.KEY_UP, true, false, 0, 13)); // ENTER
+		GameConsole.hideConsole();
+		interfc.txtPrompt.text = str + "4";
+		Lib.current.stage.dispatchEvent(new KeyboardEvent(KeyboardEvent.KEY_UP, true, false, 0, 13)); // ENTER
+		var strings = interfc.txtConsole.text.split('\r');
+		Assert.areEqual(": Test3", strings[strings.length - 2]);
+	}
 
-    @Test public function testRegisterFields():Void {
-        GameConsole.registerVariable(this, "i", "int");
-        GameConsole.registerVariable(this, "i", "int");
-        GameConsole.registerVariable(this, "f", "float");
-        GameConsole.registerVariable(this, "f", "float");
-        GameConsole.registerVariable(this, "s", "string");
-        GameConsole.registerVariable(this, "s", "string");
+	@Test public function testRegisterFields():Void {
+		GameConsole.registerVariable(this, "i", "int");
+		GameConsole.registerVariable(this, "i", "int");
+		GameConsole.registerVariable(this, "f", "float");
+		GameConsole.registerVariable(this, "f", "float");
+		GameConsole.registerVariable(this, "s", "string");
+		GameConsole.registerVariable(this, "s", "string");
 
-        GameConsole.showConsole();
+		GameConsole.showConsole();
 
-        interfc.txtPrompt.text = "set int 1234";
-        Lib.current.stage.dispatchEvent(new KeyboardEvent(KeyboardEvent.KEY_UP, true, false, 0, 13)); // ENTER
+		interfc.txtPrompt.text = "set int 1234";
+		Lib.current.stage.dispatchEvent(new KeyboardEvent(KeyboardEvent.KEY_UP, true, false, 0, 13)); // ENTER
 
-        interfc.txtPrompt.text = "set float 0.1234";
-        Lib.current.stage.dispatchEvent(new KeyboardEvent(KeyboardEvent.KEY_UP, true, false, 0, 13)); // ENTER
+		interfc.txtPrompt.text = "set float 0.1234";
+		Lib.current.stage.dispatchEvent(new KeyboardEvent(KeyboardEvent.KEY_UP, true, false, 0, 13)); // ENTER
 
-        interfc.txtPrompt.text = "set string string";
-        Lib.current.stage.dispatchEvent(new KeyboardEvent(KeyboardEvent.KEY_UP, true, false, 0, 13)); // ENTER
+		interfc.txtPrompt.text = "set string string";
+		Lib.current.stage.dispatchEvent(new KeyboardEvent(KeyboardEvent.KEY_UP, true, false, 0, 13)); // ENTER
 
-        GameConsole.clearConsole();
+		GameConsole.clearConsole();
 
-        interfc.txtPrompt.text = "vars";
-        Lib.current.stage.dispatchEvent(new KeyboardEvent(KeyboardEvent.KEY_UP, true, false, 0, 13)); // ENTER
+		interfc.txtPrompt.text = "vars";
+		Lib.current.stage.dispatchEvent(new KeyboardEvent(KeyboardEvent.KEY_UP, true, false, 0, 13)); // ENTER
 
-        var ai = (~/(int).+(1234)/).split(interfc.txtConsole.text);
-        var af = (~/(float).+(0.1234)/).split(interfc.txtConsole.text);
-        var as = (~/(string).+(string)/).split(interfc.txtConsole.text);
+		var ai = (~/(int).+(1234)/).split(interfc.txtConsole.text);
+		var af = (~/(float).+(0.1234)/).split(interfc.txtConsole.text);
+		var as = (~/(string).+(string)/).split(interfc.txtConsole.text);
 
-        Assert.isTrue(ai.length == 2 && af.length == 2 && as.length == 2);
-    }
+		Assert.isTrue(ai.length == 2 && af.length == 2 && as.length == 2);
+	}
 
 	@Test public function testAutocompleteMainCommands():Void {
 		GameConsole.showConsole();
 		interfc.txtPrompt.text = "c";
 		Lib.current.stage.dispatchEvent(new KeyboardEvent(KeyboardEvent.KEY_UP, true, false, 0, Keyboard.TAB)); // ENTER
 		Assert.areEqual("clear", interfc.txtPrompt.text);
+		Lib.current.stage.dispatchEvent(new KeyboardEvent(KeyboardEvent.KEY_UP, true, false, 0, Keyboard.TAB)); // ENTER
+		Assert.areEqual("commands", interfc.txtPrompt.text);
+	}
 
+	@Test public function testAutocompleteRegisteredCommands():Void {
+		GameConsole.showConsole();
+		GameConsole.registerFunction(this, "emptyFunc", "empty");
+		interfc.txtPrompt.text = "e";
+		Lib.current.stage.dispatchEvent(new KeyboardEvent(KeyboardEvent.KEY_UP, true, false, 0, Keyboard.TAB)); // ENTER
+		Assert.areEqual("empty", interfc.txtPrompt.text);
+	}
 
+	@Test public function testAutocompleteArguments():Void {
+		GameConsole.showConsole();
+		GameConsole.registerFunction(this, "emptyFunc", "empty",
+		function(s:String) {
+			return ["foo", "bar"];
+		});
+		interfc.txtPrompt.text = "empty ";
+		Lib.current.stage.dispatchEvent(new KeyboardEvent(KeyboardEvent.KEY_UP, true, false, 0, Keyboard.TAB)); // ENTER
+		Assert.areEqual("empty foo", interfc.txtPrompt.text);
+		Lib.current.stage.dispatchEvent(new KeyboardEvent(KeyboardEvent.KEY_UP, true, false, 0, Keyboard.TAB)); // ENTER
+		Assert.areEqual("empty bar", interfc.txtPrompt.text);
+	}
+
+	private function emptyFunc():Void {
 
 	}
 
-    private function clearText() {
-        interfc.txtConsole.text = '';
-        interfc.txtPrompt.text = '';
-        interfc.txtMonitorLeft.text = '';
-        interfc.txtMonitorRight.text = '';
-    }
+	private function clearText() {
+		interfc.txtConsole.text = '';
+		interfc.txtPrompt.text = '';
+		interfc.txtMonitorLeft.text = '';
+		interfc.txtMonitorRight.text = '';
+	}
 }
